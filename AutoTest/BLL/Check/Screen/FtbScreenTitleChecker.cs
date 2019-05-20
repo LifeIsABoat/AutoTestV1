d83715db13275ff38ba552095d080d01b6a60541
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-20 13:20:59 +0800

    Update FtbScreenTitleChecker.cs

---
+++ AutoTest/BLL/Check/Screen/FtbScreenTitleChecker.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tool.DAL;
+
+namespace Tool.BLL.Check
+{
+    class FtbScreenTitleChecker : AbstractChecker
+    {
+        public override void check()
+        {
+
+        }
+    }
+}
