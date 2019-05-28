bd27af5abd95d9cc22fe421658ea915a2a96f407
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 12:53:18 +0800

    Update EngineCommand.cs

--- AutoTest/Engine/OptionOperator/EngineCommand.cs
+++ AutoTest/Engine/OptionOperator/EngineCommand.cs
@@ -1,1 +1,15 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool.Engine
+{
+    interface IOptionOperateAPI
+    {
+        string setOption(List<string> path,string optionWords);
+        string getOption(List<string> path);
+        void setMachineIP(string ip);
+    }
+}
