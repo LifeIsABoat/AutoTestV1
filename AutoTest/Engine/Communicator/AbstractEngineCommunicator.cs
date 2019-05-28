ba1009e90f32ba87092dd2b63ba669550beb258f
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 12:51:40 +0800

    Update AbstractEngineCommunicator.cs

--- AutoTest/Engine/Communicator/AbstractEngineCommunicator.cs
+++ AutoTest/Engine/Communicator/AbstractEngineCommunicator.cs
@@ -1,1 +1,13 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool.Engine
+{
+    abstract class AbstractEngineCommunicator
+    {
+        public abstract object execute(EngineCommand command, int timeout = -1);
+    }
+}
