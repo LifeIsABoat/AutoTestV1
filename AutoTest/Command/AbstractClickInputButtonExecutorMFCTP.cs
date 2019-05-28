2c3db5c5abbc3f41a19a5522d39638f5028e0a98
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:30:41 +0800

    Update AbstractClickInputButtonExecutorMFCTP.cs

--- AutoTest/Command/AbstractClickInputButtonExecutorMFCTP.cs
+++ AutoTest/Command/AbstractClickInputButtonExecutorMFCTP.cs
@@ -1,20 +1,72 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    abstract class AbstractClickInputButtonExecutorMFCTP : AbstractCommandExecutor
     {
-        public Form1()
+        protected AbstractCommandExecutor tpClicker;
+        protected AbstractCommandExecutor fixedScreenLoader;
+
+        protected AbstractClickInputButtonExecutorMFCTP()
         {
-            InitializeComponent();
+            this.fixedScreenLoader = StaticCommandExecutorList.get(CommandList.list_f);
+            this.tpClicker = StaticCommandExecutorList.get(CommandList.click_p);
         }
-    }
+
+        public override void execute(object param)
+        {
+            if (param is string)
+            {
+                //example:inputButtonWord = "QWERASDF"
+                string inputBtnWordStr = param as string;
+                if (inputBtnWordStr != null)
+                {
+                    //start click softkeyButton
+                    StaticLog4NetLogger.commandExecutorLogger.Info("click-s \"" + inputBtnWordStr + "\" start.");
+                    try
+                    {
+                        for (int i = 0; i < inputBtnWordStr.Count(); i++)
+                        {
+                            string softkeyButton = inputBtnWordStr[i].ToString();
+                            click(softkeyButton);
+                            StaticLog4NetLogger.commandExecutorLogger.Info("click-s \"" + softkeyButton + "\" succeed.");
+                        }
+                    }
+                    catch (FTBAutoTestException excp)
+                    {
+                        StaticLog4NetLogger.commandExecutorLogger.Warn("click-s \"" + inputBtnWordStr + "\" failed.\nReason:" + excp.Message);
+                        throw excp;
+                    }
+                }
+            }
+            else if (param is ControlSoftkeyStatus)
+            {
+                ControlSoftkeyStatus oneSpecialBtn = (ControlSoftkeyStatus)Enum.Parse(typeof(ControlSoftkeyStatus), param.ToString());
+
+                StaticLog4NetLogger.commandExecutorLogger.Info("click-s \"" + oneSpecialBtn + "\" start.");
+                try
+                {
+                    specialBtnClick(oneSpecialBtn);
+                    StaticLog4NetLogger.commandExecutorLogger.Info("click-s \"" + oneSpecialBtn + "\" succeed.");
+                }
+                catch (FTBAutoTestException excp)
+                {
+                    StaticLog4NetLogger.commandExecutorLogger.Warn("click-s \"" + oneSpecialBtn + "\" failed.\nReason:" + excp.Message);
+                    throw excp;
+                }
+            }
+            else
+            {
+                throw new FTBAutoTestException("Execute error,Maybe param is null or other.");
+            }
+        }//end public override void execute()
+
+        protected abstract void click(string softkeyInputBtnWord);
+        protected abstract void specialBtnClick(ControlSoftkeyStatus oneSpecialBtn);
+    }//end abstract class AbstractClickInputButtonExecutorMFCTP
 }
