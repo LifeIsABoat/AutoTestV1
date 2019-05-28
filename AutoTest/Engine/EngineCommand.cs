5dfa7917b32e9c2422aec54f85f0344f2b9d65e3
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 12:54:05 +0800

    Update EngineCommand.cs

--- AutoTest/Engine/EngineCommand.cs
+++ AutoTest/Engine/EngineCommand.cs
@@ -1,1 +1,29 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool.Engine
+{
+    class EngineCommand
+    {
+        public string name { get; set; }
+        public object param { get; set; }
+
+        public EngineCommand()
+        {
+            name = "";
+            param = null;
+        }
+
+        public bool isValid()
+        {
+            if (null == name || "" == name)
+                return false;
+            if (null == param)
+                return false;
+            return true;
+        }
+    }
+}
