06b119fa76e0dc370468a9527255d175b9f29497
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 12:49:14 +0800

    Update AbstractMoveToScreenExecutorMFCTPByIdentifyList.cs

--- AutoTest/Command/AbstractMoveToScreenExecutorMFCTPByIdentifyList.cs
+++ AutoTest/Command/AbstractMoveToScreenExecutorMFCTPByIdentifyList.cs
@@ -1,20 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    abstract class AbstractMoveToScreenExecutorMFCTPByIdentifyList : AbstractCommandExecutor
     {
-        public Form1()
+        protected AbstractCommandExecutor tpClicker;
+        protected AbstractCommandExecutor fixedScreenLoader;
+
+        public AbstractMoveToScreenExecutorMFCTPByIdentifyList()
         {
-            InitializeComponent();
+            this.fixedScreenLoader = StaticCommandExecutorList.get(CommandList.list_f);
+            this.tpClicker = StaticCommandExecutorList.get(CommandList.click_b);  
         }
+
+        public override void execute(object param)
+        {
+            List<ControlButtonIdentify> ButtonIdList = param as List<ControlButtonIdentify>;
+            if (null == ButtonIdList)
+                throw new FTBAutoTestException("Move to target screen error by invalid param.");
+
+            StaticLog4NetLogger.commandExecutorLogger.Info("move " + ButtonIdList.ToString() + " start.");
+            try
+            {
+                change(ButtonIdList);
+                StaticLog4NetLogger.commandExecutorLogger.Info("move " + ButtonIdList.ToString() + " succeed.");
+            }
+            catch (FTBAutoTestException excp)
+            {
+                StaticLog4NetLogger.commandExecutorLogger.Warn("move " + ButtonIdList.ToString() + " failed.\nReason:" + excp.Message);
+                throw excp;
+            }
+        }
+
+        protected abstract void change(List<ControlButtonIdentify> ButtonIdentify);
     }
 }
