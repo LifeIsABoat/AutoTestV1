0caeca1b3c1653668b94ac7a9deb9ed05cdff4f8
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:23:14 +0800

    Update ClickButtonIdExecutorByFixAllFirst.cs

--- AutoTest/Command/Implementation/ClickButtonIdExecutorByFixAllFirst.cs
+++ AutoTest/Command/Implementation/ClickButtonIdExecutorByFixAllFirst.cs
@@ -1,20 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    class ClickButtonIdExecutorByFixAllFirst : AbstractClickButtonIdExecutorMFCTP
     {
-        public Form1()
+        protected override void click(ControlButtonIdentify targetButtonIdentify)
         {
-            InitializeComponent();
+            Screen currentScreen = StaticCurrentScreen.get();
+
+            string expMsg = string.Format("Can't find [{0}] button in current screen.", targetButtonIdentify.btnWordsStr);
+
+            ControlButton targetButton = currentScreen.findButton(targetButtonIdentify);
+            if (null == targetButton)
+                throw new FTBAutoTestException(expMsg);
+            if (null == targetButton.imageList || 1 != targetButton.imageList.Count)
+                throw new FTBAutoTestException(expMsg);
+
+            if (null == targetButton.imageList[0].rect)
+                throw new FTBAutoTestException(expMsg);
+            Position targetPos = targetButton.imageList[0].rect.getCenter();
+            tpClicker.execute(targetPos);
         }
     }
 }
