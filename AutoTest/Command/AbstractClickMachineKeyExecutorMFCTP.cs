ee8dd9a47bfe19741938dbde71f10694ad9834d1
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:31:19 +0800

    Update AbstractClickMachineKeyExecutorMFCTP.cs

--- AutoTest/Command/AbstractClickMachineKeyExecutorMFCTP.cs
+++ AutoTest/Command/AbstractClickMachineKeyExecutorMFCTP.cs
@@ -1,20 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Threading;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    abstract class AbstractClickMachineKeyExecutorMFCTP : AbstractCommandExecutor
     {
-        public Form1()
+        public override void execute(object param)
         {
-            InitializeComponent();
+            string keyCode = param as string;
+            if (null == keyCode)
+                throw new FTBAutoTestException("Click key error by invalid param.");
+
+            try
+            {
+                cmdMutex.WaitOne();
+                StaticLog4NetLogger.commandExecutorLogger.Info("click-k \"" + keyCode + "\" start.");
+                Thread.Sleep(300);
+                click(keyCode);
+                StaticLog4NetLogger.commandExecutorLogger.Info("click-k \"" + keyCode + "\" succeed.");
+                cmdMutex.ReleaseMutex();
+            }
+            catch (FTBAutoTestException excp)
+            {
+                StaticLog4NetLogger.commandExecutorLogger.Warn("click-k \"" + keyCode + "\" failed.\nReason:" + excp.Message);
+                cmdMutex.ReleaseMutex();
+                throw excp;
+            }
         }
+
+        protected abstract void click(string keyCode);
     }
 }
