8c7043b5d5553b3f6bcdb3387404f13bd827bbeb
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:23:51 +0800

    Update ClickButtonWordExecutorByRawScreen.cs

--- AutoTest/Command/Implementation/ClickButtonWordExecutorByRawScreen.cs
+++ AutoTest/Command/Implementation/ClickButtonWordExecutorByRawScreen.cs
@@ -1,20 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    class ClickButtonWordExecutorByRawScreen : AbstractClickButtonWordExecutorMFCTP
     {
-        public Form1()
+        protected override void click(string targetButtonWords)
         {
-            InitializeComponent();
+            Screen currentScreen = StaticCurrentScreen.get();
+
+            string expMsg = string.Format("Can't find [{0}] button in current screen.", targetButtonWords);
+
+            ElementString buttonStringElement = new ElementString(targetButtonWords);
+            AbstractElement targetStringElement = currentScreen.findElement(buttonStringElement);
+            if (null == targetStringElement)
+                throw new FTBAutoTestException(expMsg);
+            List<AbstractElement> targetImageElementList = currentScreen.getElementShipValueList(targetStringElement);
+            if (null == targetImageElementList || 1 != targetImageElementList.Count)
+                throw new FTBAutoTestException(expMsg);
+            AbstractElement targetImageElement = targetImageElementList[0];
+
+            if (null == targetImageElement.rect)
+                throw new FTBAutoTestException(expMsg);
+            Position targetPos = targetImageElement.rect.getCenter();
+            tpClicker.execute(targetPos);
         }
     }
 }
