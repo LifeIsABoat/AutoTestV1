cfe36d0d5a6f0570b66090a8d1eccbeb9dbd94b0
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:26:39 +0800

    Update ListFixedScreenExecutorMFCTPByLog.cs

--- AutoTest/Command/Implementation/ListFixedScreenExecutorMFCTPByLog.cs
+++ AutoTest/Command/Implementation/ListFixedScreenExecutorMFCTPByLog.cs
@@ -1,20 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    //-=-=-=-=-=-=-=-=-=-=-=-=Parse Logic By Log-=-=-=-=-=-=-=-=-=-=-=-=
+    //               Label/Title |  StrBtn/Tag  | Icon/Arrow
+    //ElementImage :               05   06 07 08  09 10 11 12
+    //ElementString: 01 02 03 04  05 05 06 07 08
+
+    class ListFixedScreenExecutorMFCTPByLog : AbstractListFixedScreenExecutorMFCTP
     {
-        public Form1()
+        //Use as CommonSourseData for fix Logic
+        static List<AbstractControlFixer> fixerList = new List<AbstractControlFixer>();//todo
+
+        public ListFixedScreenExecutorMFCTPByLog(LogScreen logScreen)
+            :base()
         {
-            InitializeComponent();
+            rawScreenLoader = new ListRawScreenExecutorMFCTPByLog(logScreen);//todo
+            //Initial fixer
+            fixerList.Add(new ControlInputFixer(logScreen));
+            fixerList.Add(new ControlOptionIconFixerByLog(logScreen));
+            fixerList.Add(new ControlTagFixerByLog(logScreen));
+            fixerList.Add(new ControlArrowFixerByLog(logScreen));
+            fixerList.Add(new ControlButtonFixerByLog(logScreen));
+            fixerList.Add(new ControlTitleFixer());
+            fixerList.Add(new ControlLableFixer());
+        }
+
+        protected override void fixScreen(Screen screen)
+        {
+            foreach (AbstractControlFixer fixer in fixerList)
+                fixer.fix(screen);
         }
     }
 }
