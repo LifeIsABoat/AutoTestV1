de86a3deed970319dec032c25b2aeea6942a23f4
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 12:52:11 +0800

    Update EngineCamera.cs

--- AutoTest/Engine/EngineEntity/EngineCamera.cs
+++ AutoTest/Engine/EngineEntity/EngineCamera.cs
@@ -1,1 +1,56 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tool.Engine
+{
+    class EngineCamera
+    {
+        static private AbstractEngineCommunicator engineCommunicator;
+
+        static public void setEngineCommunicator(AbstractEngineCommunicator engineCommunicator)
+        {
+            EngineCamera.engineCommunicator = engineCommunicator;
+        }
+        static public AbstractEngineCommunicator getEngineCommunicator()
+        {
+            return engineCommunicator;
+        }
+
+        //SetCameraConfig
+        public static void start(string configFilePath, int timeout = -1)
+        {
+            EngineCommand cmd = new EngineCommand();
+            cmd.name = "SetCameraConfig";
+            cmd.param = configFilePath;
+            string result = engineCommunicator.execute(cmd, timeout) as string;
+            if (null != result && "OK" == result)
+                return;
+            throw new FTBAutoTestException("Start camera engine failed.");
+        }
+        public void capture(string imagePath, int timeout = -1)
+        {
+            Thread.Sleep(500);//todo
+            EngineCommand cmd = new EngineCommand();
+            cmd.name = "CaptureScreen";
+            cmd.param = imagePath;
+            string result = engineCommunicator.execute(cmd, timeout) as string;
+            if (null != result && "OK" == result)
+                return;
+            throw new FTBAutoTestException("Camera engine capture screen failed.");
+        }
+        public static void stop(int timeout = -1)
+        {
+            EngineCommand cmd = new EngineCommand();
+            cmd.name = "HaltCameraApp";
+            cmd.param = "";
+            string result = engineCommunicator.execute(cmd, timeout) as string;
+            if (null != result && "OK" == result)
+                return;
+            throw new FTBAutoTestException("Stop camera engine failed.");
+        }
+    }
+}
