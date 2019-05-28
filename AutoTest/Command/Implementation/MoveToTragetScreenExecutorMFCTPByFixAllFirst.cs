4d30742e410efc3511e6bd601874d0f1fd0722da
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:28:22 +0800

    Update MoveToTragetScreenExecutorMFCTPByFixAllFirst.cs

--- AutoTest/Command/Implementation/MoveToTragetScreenExecutorMFCTPByFixAllFirst.cs
+++ AutoTest/Command/Implementation/MoveToTragetScreenExecutorMFCTPByFixAllFirst.cs
@@ -1,20 +1,80 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    class MoveToTragetScreenExecutorMFCTPByFixAllFirst : AbstractMoveToTragetScreenExecutorMFCTP
     {
-        public Form1()
+
+        protected override void change(List<string> btnWordsList)
         {
-            InitializeComponent();
+            if (null == btnWordsList)
+                throw new FTBAutoTestException("Can't change screen by null btnWordsList");
+
+            for (int i = 0; i < btnWordsList.Count; i++)
+            {
+                if (0 == i)
+                {
+                    homeScreenChanger.execute();
+                    continue;
+                }
+
+                AbstractScreenAggregate screenAggregate = null;
+                Screen currentScreen = new Screen();
+                fixedScreenLoader.execute(currentScreen);
+                if (currentScreen.isScrollable())
+                {
+                    screenAggregate = AbstractScreenAggregate.import(currentScreen);
+                    currentScreen = screenAggregate.toFirstScreen(currentScreen);
+                    while (!screenAggregate.isScreenContains(currentScreen))
+                    {
+                        screenAggregate.appendScreen(currentScreen);
+                        screenAggregate.moveToNextScreen(currentScreen);
+                        currentScreen = new Screen();
+                        fixedScreenLoader.execute(currentScreen);
+                    }
+                }
+
+                if (null == screenAggregate)
+                {
+                    try
+                    {
+                        btnClicker.execute(btnWordsList[i]);
+                    }
+                    catch (FTBAutoTestException)
+                    {
+                        string expMsg = string.Format("Move to target screen error by can't find [{0}] button in current screen.", btnWordsList[i]);
+                        throw new FTBAutoTestException(expMsg);
+                    }
+                }
+                else
+                {
+                    IIterator screenShowIterator;
+                    screenShowIterator = screenAggregate.createShowIterator();
+                    screenShowIterator.first();
+                    while (!screenShowIterator.isDone())
+                    {
+                        try
+                        {
+                            btnClicker.execute(btnWordsList[i]);
+                            break;
+                        }
+                        catch (FTBAutoTestException) { }
+                        screenShowIterator.next();
+                    }
+                    if (screenShowIterator.isDone())
+                    {
+                        string expMsg = string.Format("Move to target screen error by can't find [{0}] button in current screen.", btnWordsList[i]);
+                        throw new FTBAutoTestException(expMsg);
+                    }
+                    else
+                        continue;
+                }
+            }
         }
     }
 }
