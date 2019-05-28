143da83963ee00c2b741fbf7b770a3c900834446
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-28 09:28:09 +0800

    Update MoveToScreenExecutorMFCTPByIdentifyList.cs

--- AutoTest/Command/Implementation/MoveToScreenExecutorMFCTPByIdentifyList.cs
+++ AutoTest/Command/Implementation/MoveToScreenExecutorMFCTPByIdentifyList.cs
@@ -1,20 +1,66 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Tool.BLL;
 
-namespace AutoTest
+namespace Tool.Command
 {
-    public partial class Form1 : Form
+    class MoveToScreenExecutorMFCTPByIdentifyList : AbstractMoveToScreenExecutorMFCTPByIdentifyList
     {
-        public Form1()
+        protected override void change(List<ControlButtonIdentify> ButtonIdentify)
         {
-            InitializeComponent();
+            if (null == ButtonIdentify)
+                throw new FTBAutoTestException("Can't change screen by null btnWordsList");
+
+            foreach (ControlButtonIdentify ButtonWords in ButtonIdentify)
+            {
+                AbstractScreenAggregate screenAggregate = null;
+                Screen currentScreen = new Screen();
+                fixedScreenLoader.execute(currentScreen);
+                if (currentScreen.isScrollable())
+                {
+                    screenAggregate = AbstractScreenAggregate.import(currentScreen);
+                    while (!screenAggregate.isScreenContains(currentScreen))
+                    {
+                        screenAggregate.appendScreen(currentScreen);
+                        screenAggregate.moveToNextScreen(currentScreen);
+                        currentScreen = new Screen();
+                        fixedScreenLoader.execute(currentScreen);
+                    }
+                }
+
+                if (null == screenAggregate)
+                {
+                    tpClicker.execute(ButtonWords);
+                    continue;
+                }
+                else
+                {
+                    IIterator screenShowIterator;
+                    screenShowIterator = screenAggregate.createShowIterator();
+                    screenShowIterator.first();
+                    while (!screenShowIterator.isDone())
+                    {
+                        int index = screenShowIterator.currentItem();
+                        try
+                        {
+                            tpClicker.execute(ButtonWords);
+                            break;
+                        }
+                        catch (FTBAutoTestException) { }
+                        screenShowIterator.next();
+                    }
+                    if (screenShowIterator.isDone())
+                    {
+                        string expMsg = string.Format("Can't find [{0}] button in current screen.", ButtonWords.btnWordsStr);
+                        throw new FTBAutoTestException(expMsg);
+                    }
+                    else
+                        continue;
+                }
+            }
         }
     }
 }
