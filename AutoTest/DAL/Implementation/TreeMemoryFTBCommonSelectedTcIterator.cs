de60eb563e5b0ca5f214dc255b17648e7057464b
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:51:47 +0800

    Update TreeMemoryFTBCommonSelectedTcIterator.cs

--- AutoTest/DAL/Implementation/TreeMemoryFTBCommonSelectedTcIterator.cs
+++ AutoTest/DAL/Implementation/TreeMemoryFTBCommonSelectedTcIterator.cs
@@ -1,20 +1,82 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
+    class TreeMemoryFTBCommonSelectedTcIterator : IIterator
     {
-        public Form1()
+         public int sortedTcIndex { get; set; }
+        private TreeMemoryFTBCommonAggregate treeMemoryAggregate;
+
+        public TreeMemoryFTBCommonSelectedTcIterator(TreeMemoryFTBCommonAggregate aggregate)
         {
-            InitializeComponent();
+            treeMemoryAggregate = aggregate;
+            updateLevelList();
+        }
+
+        void IIterator.first()
+        {
+            sortedTcIndex = 0;
+            updateLevelList();
+        }
+
+        void IIterator.next()
+        {
+            if (sortedTcIndex < TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes.Count)
+            {
+                sortedTcIndex++;
+                if (sortedTcIndex < TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes.Count)
+                {
+                    updateLevelList();
+                }
+            }
+        }
+        bool IIterator.isDone()
+        {
+            if (sortedTcIndex >= TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        int IIterator.currentItem()
+        {
+            //return sortedTcIndex;
+            return TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes[sortedTcIndex];
+        }
+
+
+        /*
+         *  Description: Update the contents of the level index
+         *  Return: 
+         *  Exception: 
+         *  Example: updateLevelList();
+         */
+        private void updateLevelList()
+        {
+            treeMemoryAggregate.levelCompositeIndexes.Clear();
+            if (TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes.Count == 0)
+            {
+                return;
+            }
+            int tcIndex = TreeMemoryFTBCommonAggregate.sortedTcLeafIndexes[sortedTcIndex];
+            if(tcIndex < 0)
+            {
+                return;
+            }
+            for (AbstractScreenComponent node = TreeMemoryFTBCommonAggregate.tcLeafIndexes[tcIndex];
+                    node.parents != null; node = node.parents)
+            {
+                treeMemoryAggregate.levelCompositeIndexes.Add(node);
+            }
+            treeMemoryAggregate.levelCompositeIndexes.Reverse();
         }
     }
 }
