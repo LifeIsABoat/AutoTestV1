0df890ffcf036d54146160decb724f994f4bc4f9
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:47:33 +0800

    Update IFTBCommonAggregate.cs

--- AutoTest/DAL/IFTBCommonAggregate.cs
+++ AutoTest/DAL/IFTBCommonAggregate.cs
@@ -1,20 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
+    /*
+     *  Description: IFTBCommonAggregate class interface
+     */
+    interface IFTBCommonAggregate
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        /*
+         *  Description: Loaded the Aggregateof data
+         *  Return: 
+         *  Exception: 
+         *  Example:aggregate.import();
+         */
+        void importTree();
+        void importScreenDict();
+
+        /*
+         *  Description: Create an iterator
+         *  Param: type - iterator type
+         *  Return: IIterator
+         *  Exception: 
+         *  Example: tcIterator = aggregate.createMccFilteredTcIterator(type);
+         */
+        IIterator createMccFilteredTcIterator();
+        
+        /*
+         *  Description: Create an iterator
+         *  Param: type - iterator type
+         *  Return: IIterator
+         *  Exception: 
+         *  Example: levelIterator = aggregate.creatLevelIterator(type);
+         */
+        IIterator createLevelIterator();
+        /*
+         *  Description: Create an iterator
+         *  Param: type - iterator type
+         *  Return: IIterator
+         *  Exception: 
+         *  Example: tcIterator = aggregate.createSelectedTcIterator(type);
+         */
+        IIterator createSelectedTcIterator();
     }
 }
