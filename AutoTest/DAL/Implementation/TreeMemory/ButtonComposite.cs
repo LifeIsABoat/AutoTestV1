3c8281b620f19226ca622dd0c0bca9651e9db105
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:54:10 +0800

    Update ButtonComposite.cs

--- AutoTest/DAL/Implementation/TreeMemory/ButtonComposite.cs
+++ AutoTest/DAL/Implementation/TreeMemory/ButtonComposite.cs
@@ -1,20 +1,55 @@
-﻿using System;
+/*
+ * File Name: Node.cs
+ * Creat Date: 2017-3-22
+ * Description: 
+ * Modify Content:
+ */
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
+    /*
+     * Description: Node type of tree
+     */
+    public class ButtonComposite : AbstractScreenComponent
     {
-        public Form1()
+        //Objects of type FTBLableInfo
+        public LableInfo ftbButtonTitle;
+        //For its own type list
+        public List<AbstractScreenComponent> screenComponentList = new List<AbstractScreenComponent>();
+        public ButtonComposite()
+            : base()
+        { }
+
+        /* 
+         *  Description:The constructor
+         *  Param:ftbButton - Objects of type FTBButtonInfo assignment to ftbButton
+         *  Param:ftbButtonTitle - Objects of type FTBLableInfo assignment to ftbButtonTitle
+         *  Return:
+         *  Exception:
+         *  Example:
+         */
+        public ButtonComposite(ButtonInfo ftbButton, LableInfo ftbButtonTitle = null)
+            : base(ftbButton)
         {
-            InitializeComponent();
+            this.ftbButtonTitle = ftbButtonTitle;
         }
-    }
+
+        /* 
+         *  Description: Add a node to a linked list
+         *  Param: screenComponent - Node to add to list
+         *  Return:
+         *  Exception:
+         *  Example:addScreenComponent(screenComponent);
+         */
+        public override void addScreenComponent(AbstractScreenComponent screenComponent)
+        {
+            screenComponent.parents = this;
+            screenComponentList.Add(screenComponent);
+        }
+    }//class
 }
