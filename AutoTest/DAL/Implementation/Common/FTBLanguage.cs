51bb3123b75dce01a4905988764960172a5a49f6
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:53:20 +0800

    Update FTBLanguage.cs

--- AutoTest/DAL/Implementation/Common/FTBLanguage.cs
+++ AutoTest/DAL/Implementation/Common/FTBLanguage.cs
@@ -1,20 +1,77 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
+    class Language
     {
-        public Form1()
+        private static Dictionary<string, Dictionary<string, List<string>>> modelCountryLanguageData = new Dictionary<string, Dictionary<string, List<string>>>();
+        /* 
+        *  Description:add data in mcc LanguangDate list
+        *  Param:modelname - save modelname corresponding LanguangDate
+        *  Param:supportlan - supportlanuage dictionary
+        *  Return:
+        *  Exception:
+        *  Example:ftbMcc.addlanguageData("MFC", supportlan)
+        */
+        public static void addlanguageData(string modelname, Dictionary<string, List<string>> countryLanguageData)
         {
-            InitializeComponent();
+            modelCountryLanguageData.Add(modelname, countryLanguageData);
+        }
+
+        /* 
+        *  Description:get  model list
+        *  Param:model_list - Save ModelList's key
+        *  Return:List<string>
+        *  Exception:
+        *  Example:ftbMcc.getModelname(Modelname)
+        */
+        public static List<string> getModelname()
+        {
+            List<string> Modelname = new List<string>();
+            Dictionary<string, Dictionary<string, List<string>>>.KeyCollection ModelKeys = modelCountryLanguageData.Keys;
+            foreach (string modelData in ModelKeys)
+            {
+                Modelname.Add(modelData);
+            }
+            return Modelname;
+        }
+
+        /* 
+        *  Description:get mcc language list
+        *  Param:language_list - Save languageList's key
+        *  Param:model - model's key
+        *  Param:country - country's key
+        *  Return:List<string>
+        *  Exception:
+        *  Example:ftbMcc.addlanguageData(languagelist,"MFC", "USA")
+        */
+        public static List<string> getlanguage(string model, string country)
+        {
+            List<string> languagelist = new List<string>();
+            if (modelCountryLanguageData.ContainsKey(model))
+            {
+                dynamic Supportlanguage = modelCountryLanguageData[model];
+                if (Supportlanguage.ContainsKey(country))
+                {
+                    foreach (string language in Supportlanguage[country])
+                    {
+                        languagelist.Add(language);
+                    }
+                    return languagelist;
+                }
+                else
+                {
+                    throw new FTBAutoTestException("LanguangDate not include ");
+                }
+            }
+            else
+            {
+                throw new FTBAutoTestException("LanguangDate not include ");
+            }
         }
     }
 }
