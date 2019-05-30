f893f968fb181fd9094ff5d220ce5c1e754f1622
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:54:31 +0800

    Update OptionLeaf.cs

--- AutoTest/DAL/Implementation/TreeMemory/OptionLeaf.cs
+++ AutoTest/DAL/Implementation/TreeMemory/OptionLeaf.cs
@@ -1,20 +1,50 @@
-﻿using System;
+/*
+ * File Name: Leaf.cs
+ * Creat Date: 2017-3-22
+ * Description: 
+ * Modify Content:
+ */
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
+    /*
+     * Description: The leaf of the tree
+     */
+    public class OptionLeaf : AbstractScreenComponent
     {
-        public Form1()
+        public Dictionary<string,string> tcPath;
+
+        //todo remove
+        public bool tcStatus;
+
+        //todo
+        public List<short> tcTestResult;
+        //public static Dictionary<string, string> testOpinion = new Dictionary<string, string>();
+
+        public OptionLeaf()
+            : base()
         {
-            InitializeComponent();
         }
-    }
+
+        /* 
+         *  Description:The constructor
+         *  Param:ftbButton - Objects of type FTBButtonInfo assignment to ftbButton
+         *  Return:
+         *  Exception: FTBAutoException
+         *  Example:
+         */
+        public OptionLeaf(ButtonInfo ftbButton)
+            : base(ftbButton)
+        {
+        }
+        public override void addScreenComponent(AbstractScreenComponent screenComponent)
+        {
+            throw new FTBAutoTestException("Leaf cannot add child nodes");
+        }
+    }//class
 }
