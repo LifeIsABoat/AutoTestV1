19c8e516bc34389647550f484f98b29d3b40e3b9
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-05-30 10:52:29 +0800

    Update FTBButtonInfo.cs

--- AutoTest/DAL/Implementation/Common/FTBButtonInfo.cs
+++ AutoTest/DAL/Implementation/Common/FTBButtonInfo.cs
@@ -1,20 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AutoTest
+namespace Tool.DAL
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+   /*
+    *  Description:Button Info
+    */
+    public class ButtonInfo : LableInfo
+    {       
+        public Contidion ftbContidion{ get; set; }
+           
+        public ButtonInfo(string usWords,int conditionIndex=Contidion._NOCONDITION,string functionName="", string stringId="" )
+            : base(usWords,functionName, stringId )
         {
-            InitializeComponent();
+            ftbContidion = new Contidion(conditionIndex);
         }
     }
 }
