40b2b5924166fccabb630785500548e19da1ca41
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-06-03 14:18:32 +0800

    Create run.bat

---
+++ Ikatsu/WebUI/CollectiveTest/run.bat
@@ -0,0 +1,26 @@
+pybot --outputdir .\Report\ -T ^
+TC_Base\PrepareMachineA.robot ^
+TC_Set\SetEWS_General_FAX_Copy_Print.robot ^
+TC_Set\SetEWS_Administrator.robot ^
+TC_Set\SetAddressBook.robot ^
+TC_Set\SetAddressBookGroup.robot ^
+TC_Set\SetEWS_Scan.robot ^
+TC_Set\SetProfileFTP.robot ^
+TC_Set\SetProfileSFTP.robot ^
+TC_Set\SetProfileNetwork.robot ^
+TC_Set\SetProfileSharePoint.robot ^
+TC_Set\SetProfileHTTP(S)Option.robot ^
+TC_Set\SetProfileHTTP(S).robot ^
+TC_Base\DeployExIm.robot ^
+TC_Base\PrepareMachineB.robot ^
+TC_Get\GetEWS_General_FAX_Copy_Print.robot ^
+TC_Get\GetEWS_Administrator.robot ^
+TC_Get\GetAddressBook.robot ^
+TC_Get\GetAddressBookGroup.robot ^
+TC_Get\GetEWS_Scan.robot ^
+TC_Get\GetProfileFTP.robot ^
+TC_Get\GetProfileSFTP.robot ^
+TC_Get\GetProfileNetwork.robot ^
+TC_Get\GetProfileSharePoint.robot ^
+TC_Get\GetProfileHTTP(S)Option.robot ^
+TC_Get\GetProfileHTTP(S).robot
