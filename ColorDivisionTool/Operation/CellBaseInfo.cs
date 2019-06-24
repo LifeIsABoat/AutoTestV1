163e3c25967262243e32056e354f926605f6742d
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-06-24 10:53:18 +0800

    Create CellBaseInfo.cs

---
+++ ColorDivisionTool/Operation/CellBaseInfo.cs
@@ -0,0 +1,14 @@
+
+namespace ColorDivisionTool.Operation
+{
+    public class CellBaseInfo
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }    
+        public double Color { get; set; }
+        public string Value { get; set; }
+        public string MsgNo { get; set; }
+        public string ApplyNo { get; set; }
+        public string Country { get; set; }
+    }
+}
