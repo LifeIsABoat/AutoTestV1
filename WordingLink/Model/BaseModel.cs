6e875a31a54a89bac020cf3ae2314d455336eed0
LifeIsABoat <34985608+LifeIsABoat@users.noreply.github.com>
2019-06-24 16:37:18 +0800

    Create BaseModel.cs

---
+++ WordingLink/Model/BaseModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WordingLink
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
